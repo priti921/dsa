7c051872d482de1d71de40422fc83edb538ccb69
priti <roypriti921@gmail.com>
2023-06-05 00:52:54 +0530

    added new

---
+++ sort/insertionSort.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && temp > arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
