830e339dd855d3a6b7115490c46766ec410d735b
priti <roypriti921@gmail.com>
2023-06-04 20:58:07 +0530

    added new

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -1,11 +1,15 @@
 function selectionSort(arr) {
+  //loops for each elem in array
   for (let i = 0; i < arr.length; i++) {
     let min = i;
+    //loop for finding min val on each iteration
     for (let j = i + 1; j < arr.length; j++) {
+      //sets the minimum value
       if (arr[min] > arr[j]) {
         min = j;
       }
     }
+    //adds the minimum value in order ( lest to great)
     let temp = arr[min];
     arr[min] = arr[i];
     arr[i] = temp;
