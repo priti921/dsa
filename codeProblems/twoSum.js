6f62c17a7ad02bd35c74fb885711dce52a76ad9d
Priti Roy <roypriti921@gmail.com>
2023-06-11 12:26:51 +0530

    added new stuff

--- codeProblems/twoSum.js
+++ codeProblems/twoSum.js
@@ -3,8 +3,17 @@
 //Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 function twoSum(arr, target) {
+  let map = new Map()
+
+  for (let i = 0; i < arr.length; i++) {
+    let diff = target - arr[i]
+
+    if (map.has(diff)) return [map.get(diff), i]
+    map.set(arr[i], i)
+  }
+  return [,]
 
 }
 
 
-console.log(twoSum([2, 7, 11, 15], 13))
+console.log(twoSum([2, 7, 11, 15], 9))
